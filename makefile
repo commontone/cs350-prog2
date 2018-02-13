3eb380c5913f0270afafd20cf72cf97a7aaaf085
commontone <robert@robert.red>
2018-02-13 17:40:54 -0500

    All test cases good. Sterilizing inputs.

--- makefile
+++ makefile
@@ -1,18 +1,19 @@
-all: prog2 simple
+all: prog2tree simple
 
-prog2: prog2.o
-	gcc prog2.o -o prog2 -g
+prog2tree: prog2tree.o
+	gcc prog2tree.o -o prog2tree -g
 	
-prog2.o: prog2.c
-	gcc -c prog2.c -o prog2.o -g
+prog2tree.o: prog2tree.c
+	gcc -c prog2tree.c -o prog2tree.o -g
 
 simple: simple.c
 	gcc simple.c -o simple -g
-checkmem: prog2
-	valgrind --track-origins=yes ./prog2 $(ARGS)
+	
+checkmem: prog2tree
+	valgrind --track-origins=yes ./prog2tree $(ARGS)
 
-run: prog2 simple
-	./prog2 $(ARGS)
+run: prog2tree simple
+	./prog2tree $(ARGS)
 
 clean:
-	rm *.o prog2 simple vgcore.*
+	rm *.o prog2tree simple vgcore.*
---
+++ prog2tree.c
@@ -0,0 +1,129 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+	/*printf("Hello, World!\n");*/
+	int i = 0;
+	int c = 0;
+	opterr = 0;
+	int pauses = 0;
+	int children = 1;
+	int levels = 1;
+	int stime = -1;
+
+	/* Check for a usage string option */
+	for(i=0;i<argc;i++) {
+		if(strstr(argv[i],"-u") != NULL) {
+			fprintf(stderr, "This program demonstrates creating child processes of N recursive levels and M children per level\n");
+			fprintf(stderr, "prog2tree [-u] [-N <num-levels>] [-M <num-children>] [-p] [-s <sleep-time]\n");
+			return(1);
+		}
+	}
+
+	/* Argument Parsing */
+	while((c = getopt(argc, argv, "uN:M:ps:")) != -1) {
+		switch(c)
+		{
+			case 'u':
+				/* printf("You said u. But technically we should have exited by now. Whoops.\n"); */
+				break;
+			case 'N':
+				/* printf("Levels: %s \n", optarg); */
+				levels = atoi(optarg);
+				break;
+			case 'M':
+				/* printf("Children: %s \n", optarg); */
+				children = atoi(optarg);
+				break;
+			case 'p':
+				/* printf("Pausing Requested\n"); */
+				pauses = 1;
+				break;
+			case 's':
+				/* printf("Sleep Time (s): %s\n", optarg); */
+				stime = atoi(optarg);
+				break;
+		}
+	}
+
+	/* Input Cleansing */
+	if(pauses) {
+		if(stime != -1) {
+			fprintf(stderr, "Cannot use -p and -s simultaneously.\n");
+			fprintf(stderr, "This program demonstrates creating child processes of N recursive levels and M children per level\n");
+			fprintf(stderr, "prog2tree [-u] [-N <num-levels>] [-M <num-children>] [-p] [-s <sleep-time]\n");
+			exit(1);
+		}
+	} else if(stime == -1) {
+		stime = 1;
+	}
+	if(levels > 4) {
+		fprintf(stderr, "Cannot have more than 4 levels\n");
+		fprintf(stderr, "This program demonstrates creating child processes of N recursive levels and M children per level\n");
+		fprintf(stderr, "prog2tree [-u] [-N <num-levels>] [-M <num-children>] [-p] [-s <sleep-time]\n");
+		exit(1);
+	}
+	if(children > 3) {
+		fprintf(stderr, "Cannot have more than 3 children\n");
+		fprintf(stderr, "This program demonstrates creating child processes of N recursive levels and M children per level\n");
+		fprintf(stderr, "prog2tree [-u] [-N <num-levels>] [-M <num-children>] [-p] [-s <sleep-time]\n");
+		exit(1);
+	}
+
+	char levelBuffer [8];
+	sprintf(levelBuffer, "%d", levels);
+	char childBuffer [8];
+	sprintf(childBuffer, "%d", children);
+	char stimeBuffer [8];
+	sprintf(stimeBuffer, "%d", stime);
+
+	/* Recursive Execution */
+	printf("ALIVE: Level %d process with pid=%d, child of ppid=%d\n", levels, getpid(), getppid());
+	pid_t spid = getpid();
+	if(levels>1) {
+		pid_t pid;
+		for(i=0;i<children;i++) {
+			if(getpid() == spid) {
+				pid = fork();
+			}
+		}
+		if(pid < 0) { /* error */
+			fprintf(stderr, "Fork failed");
+			return 1;
+		}
+		if(pid == 0) { /*child process */
+			/* printf("I'm the child!\n"); */
+			levels = levels - 1;
+			sprintf(levelBuffer, "%d", levels);
+
+			if(pauses) {
+				char *argv2[] = { "./prog2tree", "-M", childBuffer, "-N", levelBuffer, "-p", NULL };
+				levels = levels + 1;
+				execv("./prog2tree", argv2);
+			} else {
+				char *argv2[] = { "./prog2tree", "-M", childBuffer, "-N", levelBuffer, "-s", stimeBuffer, NULL };
+				levels = levels + 1;
+				execv("./prog2tree", argv2);
+			}
+		} else { /* Parent Process */
+			/* Parent will wait for the child to complete */
+			for(i=0;i < levels*children-1;i++) {
+				wait(NULL);
+			}
+			/* printf("Child Complete\n"); */
+		}
+	} else {
+		if(stime != -1) {
+			sleep(stime);
+		} else {
+			pause();
+		}
+	}
+	printf("EXITING: Level %d process with pid=%d, child of ppid=%d\n", levels, getpid(), getppid());
+	
+	return 0;
+}
